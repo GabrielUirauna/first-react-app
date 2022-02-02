877bddcff8e528adc0f00fe5e873d490e7972d11
Gabriel Moreira <gabrielfodauna123@gmail.com>
2022-02-02 17:06:35 -0300

    Innitial Commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import { Tweet } from "./components/Tweet"
+import { AppRoutes } from "./Routes"
+
+import './App.css'
+
+function App() {
+  return (
+    <AppRoutes />
+  )
+  
+}
+
+export default App
---
+++ react-app/src/pages/Catalog.tsx
@@ -0,0 +1,3 @@
+export function Catalog() {
+    return <h1>Catálogo</h1>
+}
